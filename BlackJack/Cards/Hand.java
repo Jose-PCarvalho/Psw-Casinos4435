04e41e67d7968b995588c52528fe41c8cd4930f5
José Pedro Carvalho <up201806263@g.uporto.pt>
2021-11-24 16:00:35 +0000

    Added Shoe (Vector of Decks) class, new methods.

--- BlackJack/Cards/Hand.java
+++ BlackJack/Cards/Hand.java
@@ -1,5 +1,57 @@
 package BlackJack.Cards;
 
-public class Hand {
+
+public class Hand{
+
+    BlackJackCard[] handCards;
+    int handValue;
+    boolean bust;
+    int numberOfCards=0;
+
+    public Hand(){
+        this.numberOfCards=0;
+        this.bust=false;
+        this.handValue=0;
+
+    }
+    
+    public void insertCard(BlackJackCard Card){
+        handCards[numberOfCards]=new BlackJackCard(Card.getSuit(), Card.getSuit(), Card.getPosition());
+        numberOfCards++;
+
+    }
+    
+    public int getHandValue(){
+        this.handValue=0;
+        boolean aceFlag=false;
+        for(int i=0;i<numberOfCards;i++){
+            this.handValue+=handCards[i].getValue();
+            if(this.handCards[i].isAce()==true){
+                aceFlag=true;
+            }
+           
+        }
+
+        if(this.handValue>21 && aceFlag==true){
+            for(int i=0;i<numberOfCards;i++){
+                if(this.handCards[i].switchAceLow()==true){
+                    this.handValue-=10;
+                    break;
+                }
+            }
+        }
+
+        return this.handValue;
+    }
+
+    public boolean isBust(){
+        this.bust=false;
+        if(getHandValue()>21){
+            this.bust=true;
+            
+        }
+        return this.bust;
+    }
+
 
 }
